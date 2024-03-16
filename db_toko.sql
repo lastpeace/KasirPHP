ce8b7d8e8c2daaeff5090fb10ec21ca393d6f72b
Justin A Smith <caesarios722@gmail.com>
2024-03-16 15:25:43 +0700

    benerin database

--- db_toko.sql
+++ db_toko.sql
@@ -14,19 +14,116 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping database structure for db_toko
+CREATE DATABASE IF NOT EXISTS `db_toko` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_toko`;
 
--- Data exporting was unselected.
+-- Dumping structure for table db_toko.barang
+CREATE TABLE IF NOT EXISTS `barang` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_barang` varchar(255) NOT NULL,
+  `id_kategori` int NOT NULL,
+  `nama_barang` text NOT NULL,
+  `merk` varchar(255) NOT NULL,
+  `harga_beli` varchar(255) NOT NULL,
+  `harga_jual` varchar(255) NOT NULL,
+  `satuan_barang` varchar(255) NOT NULL,
+  `stok` text NOT NULL,
+  `tgl_input` varchar(255) NOT NULL,
+  `tgl_update` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table db_toko.barang: ~3 rows (approximately)
+INSERT INTO `barang` (`id`, `id_barang`, `id_kategori`, `nama_barang`, `merk`, `harga_beli`, `harga_jual`, `satuan_barang`, `stok`, `tgl_input`, `tgl_update`) VALUES
+	(4, 'BR001', 10, 'Buku', 'Kampus', '5000', '10000', 'PCS', '100', '16 March 2024, 14:27', NULL),
+	(5, 'BR002', 11, 'Cuci Baju', 'Molto', '10000', '20000', 'PCS', '49', '16 March 2024, 14:28', NULL),
+	(6, 'BR003', 12, 'Monitor', 'Dell', '1500000', '2000000', 'PCS', '9', '16 March 2024, 14:29', NULL);
 
--- Data exporting was unselected.
+-- Dumping structure for table db_toko.kategori
+CREATE TABLE IF NOT EXISTS `kategori` (
+  `id_kategori` int NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_kategori`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table db_toko.kategori: ~3 rows (approximately)
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+	(10, 'ATK', '16 March 2024, 14:27'),
+	(11, 'Sabun', '16 March 2024, 14:27'),
+	(12, 'Elektronik', '16 March 2024, 14:27');
 
--- Data exporting was unselected.
+-- Dumping structure for table db_toko.login
+CREATE TABLE IF NOT EXISTS `login` (
+  `id_login` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `pass` char(32) NOT NULL,
+  `id_member` int NOT NULL,
+  PRIMARY KEY (`id_login`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table db_toko.login: ~1 rows (approximately)
+INSERT INTO `login` (`id_login`, `user`, `pass`, `id_member`) VALUES
+	(1, 'admin', '202cb962ac59075b964b07152d234b70', 1);
+
+-- Dumping structure for table db_toko.member
+CREATE TABLE IF NOT EXISTS `member` (
+  `id_member` int NOT NULL AUTO_INCREMENT,
+  `nm_member` varchar(255) NOT NULL,
+  `alamat_member` text NOT NULL,
+  `telepon` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gambar` text NOT NULL,
+  `NIK` text NOT NULL,
+  PRIMARY KEY (`id_member`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table db_toko.member: ~1 rows (approximately)
+INSERT INTO `member` (`id_member`, `nm_member`, `alamat_member`, `telepon`, `email`, `gambar`, `NIK`) VALUES
+	(1, 'Raihan Caesario Ammar Saputra', 'Jl.Swatantra V no 10 09/03 Jatirasa Jatiasih Kota Bekasi', '085795526558', 'caesarios722@gmail.com', '1710573011IMG_9002.JPG', '12314121');
+
+-- Dumping structure for table db_toko.nota
+CREATE TABLE IF NOT EXISTS `nota` (
+  `id_nota` int NOT NULL AUTO_INCREMENT,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL,
+  `periode` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_nota`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table db_toko.nota: ~0 rows (approximately)
+
+-- Dumping structure for table db_toko.penjualan
+CREATE TABLE IF NOT EXISTS `penjualan` (
+  `id_penjualan` int NOT NULL AUTO_INCREMENT,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_penjualan`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table db_toko.penjualan: ~0 rows (approximately)
+
+-- Dumping structure for table db_toko.toko
+CREATE TABLE IF NOT EXISTS `toko` (
+  `id_toko` int NOT NULL AUTO_INCREMENT,
+  `nama_toko` varchar(255) NOT NULL,
+  `alamat_toko` text NOT NULL,
+  `tlp` varchar(255) NOT NULL,
+  `nama_pemilik` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_toko`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table db_toko.toko: ~1 rows (approximately)
+INSERT INTO `toko` (`id_toko`, `nama_toko`, `alamat_toko`, `tlp`, `nama_pemilik`) VALUES
+	(1, 'HanzStore', 'Jl. Swatantra V No 10 Jatirasa Jatiasih Kota Bekasi', '085795526558', 'Ammar');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
